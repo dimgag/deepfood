5df072dfb5b3a0076c8648d0366ec4c3c969269c
dimgag <gagatsis94@gmail.com>
2022-08-23 11:05:28 +0200

    code comments

--- python/evaluate.py
+++ python/evaluate.py
@@ -1,5 +1,5 @@
-# Print training, test accuracy and loss of the model
 def model_eval(model, train, val):
+    # Print training, test accuracy and loss of the model
     # evaluate the model
     train_loss, train_acc = model.evaluate(train, verbose=0)
     val_loss, val_acc = model.evaluate(val, verbose=0)
