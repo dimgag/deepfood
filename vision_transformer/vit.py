29fd7571f6d0f3dbf0716fac5130a73ed8036d6b
dimgag <gagatsis94@gmail.com>
2022-09-05 11:29:52 +0200

    Class fix on Patches Layer

--- vision_transformer/vit.py
+++ vision_transformer/vit.py
@@ -12,46 +12,20 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 #Source: https://keras.io/examples/vision/image_classification_with_vision_transformer/
-
-
 # # Full Food-101 dataset
+# !wget https://s3.amazonaws.com/fast-ai-imageclas/food-101.tar.gz
+# !tar -xvzf food-101.tar.gz
 train_data_dir = '/Users/dim__gag/Desktop/food-101/train'
 validation_data_dir = '/Users/dim__gag/Desktop/food-101/test'
-
-
-# Hyperparameters
+# Data parameters
 num_classes = 101
-input_shape = (299, 299,3)
-learning_rate = 0.001
-weight_decay = 0.0001
-batch_size = 256
-num_epochs = 100
-image_size = 299  # We'll resize input images to this size
-patch_size = 6  # Size of the patches to be extract from the input images
-num_patches = (image_size // patch_size) ** 2
-projection_dim = 64
-num_heads = 4
-transformer_units = [
-    projection_dim * 2,
-    projection_dim,
-]  # Size of the transformer layers
-transformer_layers = 8
-mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
-
-
-
+input_shape = (299, 299, 3)
 img_width, img_height = 299, 299 # input image dimensions
+batch_size = 32 # Change this to your desired batch size
+epochs = 100 # Change the Number of epochs here
 
-nb_train_samples = 75750
-nb_validation_samples = 25250
-
-
-
-
-
-
-
-def data_augmentation_1(train_data_dir, validation_data_dir, img_height, img_width, batch_size):
+# Data Augmentation
+def data_augmentation(train_data_dir, validation_data_dir, img_height, img_width, batch_size):
     # Data Augmentation
     train_datagen = ImageDataGenerator(
         rescale=1. / 255,
@@ -76,50 +50,45 @@ def data_augmentation_1(train_data_dir, validation_data_dir, img_height, img_wid
 
 
 
+train_generator, validation_generator = data_augmentation(train_data_dir=train_data_dir, validation_data_dir=validation_data_dir, img_width=img_height, img_height=img_height, batch_size=batch_size)
 
-train_generator, validation_generator = data_augmentation_1(train_data_dir=train_data_dir, validation_data_dir=validation_data_dir, img_width=img_height, img_height=img_height, batch_size=batch_size)
-
-
-
-data_augmentation = keras.Sequential(
-    [
-        layers.Normalization(),
-        layers.Resizing(image_size, image_size),
-        layers.RandomFlip("horizontal"),
-        layers.RandomRotation(factor=0.02),
-        layers.RandomZoom(
-            height_factor=0.2, width_factor=0.2
-        ),
-    ],
-    name="data_augmentation",
-)
-# Compute the mean and the variance of the training data for normalization.
-data_augmentation.layers[0].adapt(train_generator)
+# Number of class indices
+print(train_generator.class_indices)
 
 
+# Build the ViT model
+
+# Hypererparameters
+learning_rate = 0.001
+weight_decay = 0.0001
+batch_size = 256
+num_epochs = 100
+image_size = 299  # We'll resize input images to this size
+patch_size = 6  # Size of the patches to be extract from the input images
+num_patches = (image_size // patch_size) ** 2
+projection_dim = 64
+num_heads = 4
+transformer_units = [
+    projection_dim * 2,
+    projection_dim,
+]  # Size of the transformer layers
+transformer_layers = 8
+mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
 
 
 
-
-
-
-
-# Implment multilayer perceptron classifier
 def mlp(x, hidden_units, dropout_rate):
     for units in hidden_units:
         x = layers.Dense(units, activation=tf.nn.gelu)(x)
         x = layers.Dropout(dropout_rate)(x)
     return x
 
-
-
-
-# Implement patch creation as a layer
+# Implement patch creation as a layer.
 class Patches(layers.Layer):
     def __init__(self, patch_size):
         super(Patches, self).__init__()
         self.patch_size = patch_size
-
+    # call for food-101 dataset
     def call(self, images):
         batch_size = tf.shape(images)[0]
         patches = tf.image.extract_patches(
@@ -135,108 +104,29 @@ class Patches(layers.Layer):
 
 
 
-
-# Implement Patch encoding 
-
-class PatchEncoder(layers.Layer):
-    def __init__(self, num_patches, projection_dim):
-        super(PatchEncoder, self).__init__()
-        self.num_patches = num_patches
-        self.projection = layers.Dense(units=projection_dim)
-        self.position_embedding = layers.Embedding(
-            input_dim=num_patches, output_dim=projection_dim
-        )
-
-    def call(self, patch):
-        positions = tf.range(start=0, limit=self.num_patches, delta=1)
-        encoded = self.projection(patch) + self.position_embedding(positions)
-        return encoded
+# Visualize the patches of a single image
+import matplotlib.pyplot as plt
+plt.figure(figsize=(4, 4))
+image = train_generator[0][0][0]
+plt.imshow(image)
+plt.axis("off")
+plt.show()
 
 
+resized_image = tf.image.resize(tf.convert_to_tensor([image]), size=(image_size, image_size))
 
 
-# Build the ViT model
-def create_vit_classifier():
-    inputs = layers.Input(shape=input_shape)
-    # Augment data.
-    augmented = data_augmentation(inputs)
-    # Create patches.
-    patches = Patches(patch_size)(augmented)
-    # Encode patches.
-    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+# Create the patch layer.
+patches = Patches(patch_size)(resized_image)
+print(f"Image size: {image_size} X {image_size}")
+print(f"Patch size: {patch_size} X {patch_size}")
+print(f"Patches per image: {patches.shape[1]}")
+print(f"Elements per patch: {patches.shape[-1]}")
 
-    # Create multiple layers of the Transformer block.
-    for _ in range(transformer_layers):
-        # Layer normalization 1.
-        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-        # Create a multi-head attention layer.
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
-        )(x1, x1)
-        # Skip connection 1.
-        x2 = layers.Add()([attention_output, encoded_patches])
-        # Layer normalization 2.
-        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
-        # MLP.
-        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-        # Skip connection 2.
-        encoded_patches = layers.Add()([x3, x2])
-
-    # Create a [batch_size, projection_dim] tensor.
-    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-    representation = layers.Flatten()(representation)
-    representation = layers.Dropout(0.5)(representation)
-    # Add MLP.
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
-    # Classify outputs.
-    logits = layers.Dense(num_classes)(features)
-    # Create the Keras model.
-    model = keras.Model(inputs=inputs, outputs=logits)
-    return model
-
-
-
-
-
-# Compile, train, and evaluate the model
-def run_experiment(model):
-    optimizer = tfa.optimizers.AdamW(
-        learning_rate=learning_rate, weight_decay=weight_decay
-    )
-
-    model.compile(
-        optimizer=optimizer,
-        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=[
-            keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
-            keras.metrics.SparseTopKCategoricalAccuracy(5, name="top-5-accuracy"),
-        ],
-    )
-
-    checkpoint_filepath = "/tmp/checkpoint"
-    checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        checkpoint_filepath,
-        monitor="val_accuracy",
-        save_best_only=True,
-        save_weights_only=True,
-    )
-
-    history = model.fit(
-        x=train_generator,
-        y=validation_generator,
-        batch_size=batch_size,
-        epochs=num_epochs,
-        validation_split=0.1,
-        callbacks=[checkpoint_callback],
-    )
-
-    model.load_weights(checkpoint_filepath)
-    _, accuracy, top_5_accuracy = model.evaluate(train_generator, validation_generator)
-    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
-    print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
-
-    return history
-
-
-vit_classifier = create_vit_classifier()
-history = run_experiment(vit_classifier)
+n = int(np.sqrt(patches.shape[1]))
+plt.figure(figsize=(4, 4))
+for i, patch in enumerate(patches[0]):
+    ax = plt.subplot(n, n, i + 1)
+    patch_img = tf.reshape(patch, (patch_size, patch_size, 3))
+    plt.imshow(patch_img.numpy())
+    plt.axis("off")
