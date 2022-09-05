747836d6947c35331f1d94af38a46e489cf9fffe
dimgag <gagatsis94@gmail.com>
2022-09-05 12:53:26 +0200

    model creation

--- vision_transformer/vit.py
+++ vision_transformer/vit.py
@@ -1,7 +1,6 @@
 '''
 Implementation of the ViT model in Keras for Food Classification on the Food-101 dataset.
 In this implementation, the model is trained on the Food-101 dataset and evaluated on the Food-101 test set.
-Still in progress.
 '''
 import numpy as np
 import tensorflow as tf
@@ -22,7 +21,7 @@ num_classes = 101
 input_shape = (299, 299, 3)
 img_width, img_height = 299, 299 # input image dimensions
 batch_size = 32 # Change this to your desired batch size
-epochs = 100 # Change the Number of epochs here
+epochs = 1 # Change the Number of epochs here
 
 # Data Augmentation
 def data_augmentation(train_data_dir, validation_data_dir, img_height, img_width, batch_size):
@@ -53,7 +52,7 @@ def data_augmentation(train_data_dir, validation_data_dir, img_height, img_width
 train_generator, validation_generator = data_augmentation(train_data_dir=train_data_dir, validation_data_dir=validation_data_dir, img_width=img_height, img_height=img_height, batch_size=batch_size)
 
 # Number of class indices
-print(train_generator.class_indices)
+# print(train_generator.class_indices)
 
 
 # Build the ViT model
@@ -62,7 +61,6 @@ print(train_generator.class_indices)
 learning_rate = 0.001
 weight_decay = 0.0001
 batch_size = 256
-num_epochs = 100
 image_size = 299  # We'll resize input images to this size
 patch_size = 6  # Size of the patches to be extract from the input images
 num_patches = (image_size // patch_size) ** 2
@@ -83,7 +81,7 @@ def mlp(x, hidden_units, dropout_rate):
         x = layers.Dropout(dropout_rate)(x)
     return x
 
-# Implement patch creation as a layer.
+# Implement patch creation layer
 class Patches(layers.Layer):
     def __init__(self, patch_size):
         super(Patches, self).__init__()
@@ -104,25 +102,21 @@ class Patches(layers.Layer):
 
 
 
+"""
 # Visualize the patches of a single image
 import matplotlib.pyplot as plt
 plt.figure(figsize=(4, 4))
 image = train_generator[0][0][0]
 plt.imshow(image)
 plt.axis("off")
-plt.show()
-
-
 resized_image = tf.image.resize(tf.convert_to_tensor([image]), size=(image_size, image_size))
-
-
 # Create the patch layer.
 patches = Patches(patch_size)(resized_image)
 print(f"Image size: {image_size} X {image_size}")
 print(f"Patch size: {patch_size} X {patch_size}")
 print(f"Patches per image: {patches.shape[1]}")
 print(f"Elements per patch: {patches.shape[-1]}")
-
+# Visualize the patches
 n = int(np.sqrt(patches.shape[1]))
 plt.figure(figsize=(4, 4))
 for i, patch in enumerate(patches[0]):
@@ -130,3 +124,96 @@ for i, patch in enumerate(patches[0]):
     patch_img = tf.reshape(patch, (patch_size, patch_size, 3))
     plt.imshow(patch_img.numpy())
     plt.axis("off")
+plt.show()
+"""
+
+
+# Implement the patch encoding layer
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
+
+"""
+Test the patch encoding layer:
+encoded_patches_1 = PatchEncoder(num_patches, projection_dim)(patches)
+encoded_patches_1.shape
+print(encoded_patches_1)"""
+
+# Create ViT model
+def create_vit_classifier():
+    inputs = layers.Input(shape=input_shape)
+    # Create patches.
+    patches = Patches(patch_size)(inputs)
+    # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = layers.Flatten()(representation)
+    representation = layers.Dropout(0.5)(representation)
+    # Add MLP.
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    # Classify outputs.
+    logits = layers.Dense(num_classes)(features)
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=logits)
+    return model
+
+
+# Create the ViT classifier
+vit_classifier = create_vit_classifier()
+# vit_classifier.summary()
+
+
+# Compile the model
+vit_classifier.compile(
+    optimizer=tfa.optimizers.AdamW(
+        learning_rate=learning_rate, weight_decay=weight_decay
+    ),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+)
+
+checkpoint_filepath = "~/vision_transformer/food-101-vit.h5"
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='val_accuracy',
+    save_best_only=True)
+
+
+# Train the model
+history = vit_classifier.fit(train_generator, epochs=epochs, validation_data=validation_generator, callbacks=[model_checkpoint_callback])
+
+
+# Evaluate the model
+test_loss, test_acc = vit_classifier.evaluate(test_generator, verbose=2)
+print('Validation loss:', test_loss)
+print('Validation accuracy:', test_acc)
